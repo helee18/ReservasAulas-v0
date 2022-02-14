2dca0ad41e5c175b14bcef85072267a044229bf1
heleven-pc <heleven-pc@heleven-pc>
2022-02-14 20:41:58 +0100

    correccion to string

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/Opcion.java
@@ -7,65 +7,80 @@ public enum Opcion {
 		}
 	}, 
 	INSERTAR_AULA("1.- Insertar aula"){
+		public void ejecutar() {
 		vista.insertarAula();
-	}
-}, 
+		}
+	}, 
 	BORRAR_AULA("2.- Borrar aula"){
-		vista.borrarAula();
-	}
-}, 
+		public void ejecutar() {
+			vista.borrarAula();
+		}
+	}, 
 	BUSCAR_AULA("3.- Buscar aula"){
-		vista.buscarAula();
-	}
-}, 
+		public void ejecutar() {
+			vista.buscarAula();
+		}
+	}, 
 	LISTAS_AULAS("4.- Listar aulas"){
-		vista.listarAulas();
-	}
-}, 
+		public void ejecutar() {
+			vista.listarAulas();
+		}
+	}, 
 	INSERTAR_PROFESOR("5.- Insertar profesor"){
-		vista.insertarProfesor();
-	}
-}, 
+		public void ejecutar() {
+			vista.insertarProfesor();
+		}
+	}, 
 	BORRAR_PROFESOR("6.- Borrar profesor"){
+	public void ejecutar() {
 		vista.borrarProfesor();
-	}
-}, 
+		}
+	}, 
 	BUSCAR_PROFESOR("7.- Buscar profesor"){
-		vista.buscarProfesor();
-	}
-}, 
+		public void ejecutar() {
+			vista.buscarProfesor();
+		}
+	}, 
 	LISTAR_PROFESORES("8.- Listar profesores"){
-		vista.listarProfesores();
-	}
-},
+		public void ejecutar() {
+			vista.listarProfesores();
+		}
+	},	
 	INSERTAR_RESERVA("9.- Insertar reserva"){
-		vista.insertarReserva();
-	}
-}, 
+		public void ejecutar() {
+			vista.insertarReserva();
+		}
+	}, 
 	BORRAR_RESERVA("10.- Borrar reserva"){
-		vista.borrarReserva();
-	}
-}, 
+		public void ejecutar() {
+			vista.borrarReserva();
+		}
+	}, 
 	LISTAR_RESERVAS("11.- Listar reservas"){
-		vista.listarReservas();
-	}
-}, 
+		public void ejecutar() {
+			vista.listarReservas();
+		}
+	}, 
 	LISTAR_RESERVAS_AULA("12.- Listar reservas aula"){
-		vista.listarReservasAula();
-	}
-}, 
+		public void ejecutar() {
+			vista.listarReservasAula();
+		}
+	}, 
 	LISTAR_RESERVAS_PROFESOR("13.- Listar reservas profesor"){
-		vista.listarReservasProfesor();
-	}
-}, 
+		public void ejecutar() {
+			vista.listarReservasProfesor();
+		}
+	}, 
 	LISTAR_RESERVAS_PERMANENCIA("14.- Listar reservas permanencia"){
-		vista.listarReservasPermanencia();
-	}
-},
+		public void ejecutar() {
+			vista.listarReservasPermanencia();
+		}
+	},
 	CONSULTAR_DISPONIBILIDAD("15.- Consultar disponibilidad"){
-		vista.consultarDisponibilidad();
-	}
-};
+		public void ejecutar() {
+			vista.consultarDisponibilidad();
+		}
+	};
 	
 	private String mensajeAMostrar;
 	private static Vista vista;
@@ -81,24 +96,25 @@ public enum Opcion {
 	public abstract void ejecutar();
 	
 	protected static void setVista(Vista vistaIntoducida) {
-		vista = vistaIntoducida;
+		if (vista == null) {
+			throw new NullPointerException("ERROR: La vista no pueda ser nula.");
+		}
+		Opcion.vista = vistaIntoducida;
 	}
 	
 	public String toString() {
-		return mensajeAMostrar;
+		return String.format("%d.- %s", ordinal(), mensajeAMostrar);
 	}
 	
 	public static Opcion getOpcionSegunOrdinal(int ordinal) {
-		if (!esOrdinalValido(ordinal))
-			throw new IllegalArgumentException("ERROR: No es un ordinal valido.");
-			
-		return Opcion.values()[ordinal];
+		if (!esOrdinalValido(ordinal)) {
+			throw new IllegalArgumentException("Ordinal de la opción no válido");
+		}
+		return values()[ordinal];
 	}
 
 	public static boolean esOrdinalValido(int ordinal) {
-		if(ordinal >= 0 && ordinal < Opcion.values().length)
-			return true;
-		
-		return false;
+		return (ordinal >= 0 && ordinal <= values().length - 1);
 	}
+	
 }
