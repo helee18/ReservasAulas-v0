24008e52ee0fc6b59f5b35c43d7c8e46f24fc929
heleven-pc <heleven-pc@heleven-pc>
2022-02-14 20:10:31 +0100

    cambio string opciones

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Tramo.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/dominio/Tramo.java
@@ -1,7 +1,7 @@
 package org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio;
 
 public enum Tramo {
-	MANANA("Mañana"),TARDE("Tarde"); //opciones
+	MANANA("0.- Mañana"),TARDE("1.- Tarde"); //opciones
 	
 	private String cadenaAMostrar; 
 	
