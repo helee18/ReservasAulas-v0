096b07d240ef6cdd02156ea580c5b22e8a52603b
heleven-pc <heleven-pc@192.168.22.11>
2022-02-16 23:54:52 +0100

    metodo opcion

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/Opcion.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/Opcion.java
@@ -1,89 +1,89 @@
 package org.iesalandalus.programacion.reservasaulas.mvc.vista;
 
 public enum Opcion {
-	SALIR("0.- Salir"){
+	SALIR("Salir"){
 		public void ejecutar() {
 			vista.salir();
 		}
 	}, 
-	INSERTAR_AULA("1.- Insertar aula"){
+	INSERTAR_AULA("Insertar aula"){
 		public void ejecutar() {
 		vista.insertarAula();
 		}
 	}, 
-	BORRAR_AULA("2.- Borrar aula"){
+	BORRAR_AULA("Borrar aula"){
 		public void ejecutar() {
 			vista.borrarAula();
 		}
 	}, 
-	BUSCAR_AULA("3.- Buscar aula"){
+	BUSCAR_AULA("Buscar aula"){
 		public void ejecutar() {
 			vista.buscarAula();
 		}
 	}, 
-	LISTAS_AULAS("4.- Listar aulas"){
+	LISTAS_AULAS("Listar aulas"){
 		public void ejecutar() {
 			vista.listarAulas();
 		}
 	}, 
-	INSERTAR_PROFESOR("5.- Insertar profesor"){
+	INSERTAR_PROFESOR("Insertar profesor"){
 		public void ejecutar() {
 			vista.insertarProfesor();
 		}
 	}, 
-	BORRAR_PROFESOR("6.- Borrar profesor"){
-	public void ejecutar() {
-		vista.borrarProfesor();
+	BORRAR_PROFESOR("Borrar profesor"){
+		public void ejecutar() {
+			vista.borrarProfesor();
 		}
 	}, 
-	BUSCAR_PROFESOR("7.- Buscar profesor"){
+	BUSCAR_PROFESOR("Buscar profesor"){
 		public void ejecutar() {
 			vista.buscarProfesor();
 		}
 	}, 
-	LISTAR_PROFESORES("8.- Listar profesores"){
+	LISTAR_PROFESORES("Listar profesores"){
 		public void ejecutar() {
 			vista.listarProfesores();
 		}
 	},	
-	INSERTAR_RESERVA("9.- Insertar reserva"){
+	INSERTAR_RESERVA("Insertar reserva"){
 		public void ejecutar() {
 			vista.realizarReserva();
 		}
 	}, 
-	BORRAR_RESERVA("10.- Borrar reserva"){
+	BORRAR_RESERVA("Borrar reserva"){
 		public void ejecutar() {
 			vista.anularReserva();
 		}
 	}, 
-	LISTAR_RESERVAS("11.- Listar reservas"){
+	LISTAR_RESERVAS("Listar reservas"){
 		public void ejecutar() {
 			vista.listarReservas();
 		}
 	}, 
-	LISTAR_RESERVAS_AULA("12.- Listar reservas aula"){
+	LISTAR_RESERVAS_AULA("Listar reservas aula"){
 		public void ejecutar() {
 			vista.listarReservasAula();
 		}
 	}, 
-	LISTAR_RESERVAS_PROFESOR("13.- Listar reservas profesor"){
+	LISTAR_RESERVAS_PROFESOR("Listar reservas profesor"){
 		public void ejecutar() {
 			vista.listarReservasProfesor();
 		}
 	}, 
-	LISTAR_RESERVAS_PERMANENCIA("14.- Listar reservas permanencia"){
+	LISTAR_RESERVAS_PERMANENCIA("Listar reservas permanencia"){
 		public void ejecutar() {
 			vista.listarReservasPermanencia();
 		}
 	},
-	CONSULTAR_DISPONIBILIDAD("15.- Consultar disponibilidad"){
+	CONSULTAR_DISPONIBILIDAD("Consultar disponibilidad"){
 		public void ejecutar() {
 			vista.consultarDisponibilidad();
 		}
 	};
 	
 	private String mensajeAMostrar;
-	private static Vista vista;
+	static Vista vista;
 	
 	private Opcion(String mensajeAMostrar) {
 		this.mensajeAMostrar = mensajeAMostrar;
@@ -96,9 +96,9 @@ public enum Opcion {
 	public abstract void ejecutar();
 	
 	protected static void setVista(Vista vistaIntoducida) {
-		if (vista == null) {
+		if (vista == null) 
 			throw new NullPointerException("ERROR: La vista no pueda ser nula.");
-		}
+		
 		Opcion.vista = vistaIntoducida;
 	}
 	
